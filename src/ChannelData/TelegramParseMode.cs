34e7010d9a7ac745d2b4d3c54ce96065ce1a3e18
pmosk <pm@ggroupp.ru>
2022-06-17 21:50:14 +0300

    Fix parse mode serialization

--- src/ChannelData/TelegramParseMode.cs
+++ src/ChannelData/TelegramParseMode.cs
@@ -1,10 +1,7 @@
 using System.Runtime.Serialization;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json.Converters;
 
 namespace GGroupp.Infra.Bot.Builder;
 
-[JsonConverter(typeof(StringEnumConverter))]
 public enum TelegramParseMode
 {
     [EnumMember(Value = "MarkdownV2")]
